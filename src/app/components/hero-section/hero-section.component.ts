9b94ef41c41f80c04d67e9528de23d36c900d11d
Utkarsh Gupta <utkarsh13370@gmail.com>
2023-07-08 22:11:20 +0530

    Image array added in hero section

--- src/app/components/hero-section/hero-section.component.ts
+++ src/app/components/hero-section/hero-section.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-hero-section',
   templateUrl: './hero-section.component.html',
-  styleUrls: ['./hero-section.component.scss']
+  styleUrls: ['./hero-section.component.scss'],
 })
 export class HeroSectionComponent implements OnInit {
+  customerImgs = [
+    'customer-1',
+    'customer-2',
+    'customer-3',
+    'customer-4',
+    'customer-5',
+    'customer-6',
+  ];
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
