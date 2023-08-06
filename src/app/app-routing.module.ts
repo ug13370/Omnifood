f67bbfde82a38bf224807fbe59eeedcc18438d92
Utkarsh Gupta <utkarsh13370@gmail.com>
2023-08-06 14:58:17 +0530

    Routing added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomePageComponent },
+  { path: '**', redirectTo: 'home' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,34 @@
 import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { AppRoutingModule } from './app-routing.module';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { HeaderComponent } from './components/header/header.component';
 import { HowSectionComponent } from './components/how-section/how-section.component';
-import { FeaturedSectionComponent } from './components/featured-section/featured-section.component';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
 import { MealsSectionComponent } from './components/meals-section/meals-section.component';
-import { IonicModule } from '@ionic/angular';
-import { TestimonialsSectionComponent } from './components/testimonials-section/testimonials-section.component';
-import { PricingSectionComponent } from './components/pricing-section/pricing-section.component';
 import { FooterSectionComponent } from './components/footer-section/footer-section.component';
+import { PricingSectionComponent } from './components/pricing-section/pricing-section.component';
+import { FeaturedSectionComponent } from './components/featured-section/featured-section.component';
+import { TestimonialsSectionComponent } from './components/testimonials-section/testimonials-section.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroSectionComponent,
     HeaderComponent,
     HowSectionComponent,
-    FeaturedSectionComponent,
+    HeroSectionComponent,
+    FooterSectionComponent,
     MealsSectionComponent,
-    TestimonialsSectionComponent,
     PricingSectionComponent,
-    FooterSectionComponent
+    FeaturedSectionComponent,
+    TestimonialsSectionComponent,
+    HomePageComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    IonicModule.forRoot(),
-  ],
+  imports: [BrowserModule, AppRoutingModule, IonicModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
